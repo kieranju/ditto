9ed4db1ac573f233ef1cf6a681fe611ae26e093d
kieranju <kieranju@gmail.com>
2018-11-17 03:20:20 -0800

    Add simulated typing

--- msb/__main__.py
+++ msb/__main__.py
@@ -1,4 +1,6 @@
 import simulated
 
 # TODO: create submodule for hardware simulation
-simulated.mouse_to(500, 500)
+# simulated.mouse_to(500, 500)
+
+simulated.type('hello my dude, how\'s it goin?')
--- msb/simulated.py
+++ msb/simulated.py
@@ -33,8 +33,51 @@ def mouse_to(destination_x, destination_y, time_between_points=0.0001):
     points = scipy.interpolate.splev(u, tck)
 
     # Move mouse
-    time_gate = time.perf_counter()
+    delay = time.perf_counter()
     for point in zip(*(i.astype(int) for i in points)):
         pyautogui.moveTo(*point)
-        while time.perf_counter() < time_gate: pass
-        time_gate = time.perf_counter() + time_between_points
+        while time.perf_counter() < delay: pass
+        delay = time.perf_counter() + time_between_points
+
+# TODO: add ability to make mistakes and delete them, then continue
+def type(phrase, words_per_minute=125):
+    letter_variance_limit = 0.025  # Typing delay randomness 
+    word_delay_upper_limit = 0.4   # Highest delay for the biggest words
+    word_delay_slope = 5           # Effectiveness of a single letter on the word delay, less is more
+    symbol_delay = 0.018           # Extra delay for special characters
+    gross_word_size = 5            # Average typed characters per word
+    
+    words = phrase.split()
+    word_count = len(words)
+    gross_word_count = len(phrase) / gross_word_size
+    time_to_completion = (gross_word_count / words_per_minute) * 60  # seconds
+    average_time_per_char = time_to_completion / len(phrase)
+    delay = time.perf_counter()
+    
+    for index, word in enumerate(words, start=1):
+        word_length = len(word)
+        word_delay = (word_length / (word_length + word_delay_slope)) * word_delay_upper_limit
+
+        # Type the word
+        for char in word:
+            pyautogui.press(char)
+
+            # Introduce typing delays
+            letter_variance = random.uniform(-letter_variance_limit, letter_variance_limit)
+
+            # Check for symbols
+            char_contains_symbol = not char.isalnum()
+            if char_contains_symbol:
+                letter_variance += symbol_delay
+
+            delay = time.perf_counter() + (average_time_per_char + letter_variance)
+            while time.perf_counter() < delay: pass
+
+        # Phrase is finito
+        if index == word_count:
+            break
+
+        # Add a space, wait, then continue the phrase
+        pyautogui.press('space')
+        delay = time.perf_counter() + word_delay
+        while time.perf_counter() < delay: pass
